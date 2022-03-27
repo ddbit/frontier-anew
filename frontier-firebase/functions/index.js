b5f08d825fd99c06c83118b5d4ac5122f8b9ff32
Davide <davide@Air-di-Davide.lan>
2022-03-28 01:06:36 +0200

    deploy ok and query params ok

--- frontier-firebase/functions/index.js
+++ frontier-firebase/functions/index.js
@@ -1,11 +1,18 @@
 const functions = require("firebase-functions");
-
+const {stats} = require("./utils");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello logs!", {structuredData: true}); 
     response.setHeader("Access-Control-Allow-Origin","*");
-    response.send([{"a":1},{"a":2},{"a":3}]);   
+    response.send({"x":request.query.x});   
  });
 
+
+ exports.stats = functions.https.onRequest((request, response) => {
+    functions.logger.info("Stats 30 days!", {structuredData: true}); 
+    response.setHeader("Access-Control-Allow-Origin","*");
+    var ticker = request.query.ticker;
+    response.send({"x":ticker});
+ });
--- frontier-firebase/functions/main.js
+++ frontier-firebase/functions/main.js
@@ -1,64 +1,13 @@
-const {restClient} = require("@polygon.io/client-js");
-
-
-async function closeArray(){
- const api = restClient("who5oAF5P5GAYuy8pjWZ3dC47a9rTOP5");
-
-
- let to = new Date();
- let from = ((date) => {
-     date.setDate(to.getDate() - 30);
-     return date;
-    }
-  )(new Date());
-
- 
- console.log(to.toISOString()); 
- console.log(from.toISOString());
- // Output: Tue Jul 21 2020 10:01:14 GMT+0100 (UK Daylight Time) 
+const {stats} = require("./utils");
 
 
 
- var response = //await api.forex.previousClose("C:EURUSD");
- await api.crypto.aggregates(
-    "X:BTCUSD",
-    1,
-    "day",
-    from.toISOString().substring(0,10),
-    to.toISOString().substring(0,10),
-    true
-);
-
-let close = [];
-response.results.forEach(r => {
-    //console.log(r.c);
-    close.push(r.c);
-});
-
-console.log(close);
-
-}
-
-function addVector(a,b){
-    return a.map((e,i) => e + b[i]);
-}
-
-function mulVector(a,b){
-    return a.map((e,i) => e * b[i]);
-}
-
-function divideVector(a,b){
-    return a.map((e,i) => e / b[i]);
-}
-
-function shiftVector(a,n){
-
-}
 
 async function main(){
     console.log("main");
-    //await closeArray();
-    console.log(divideVector([1,2,3],[2,2,2]));
+    const ticker = "X:BTCUSD";
+    console.log(await stats(ticker));
+
 }
 
 main();
