1ddcebb4ea425cf255598124f64b859d84ca67d3
davide <davide@Mini-di-davide.lan>
2022-03-30 15:22:54 +0200

    new card layout

--- frontend-svelte/my-svelte-project/src/App.svelte
+++ frontend-svelte/my-svelte-project/src/App.svelte
@@ -1,34 +1,7 @@
 <script>
-	import { onMount, tick } from "svelte";
-    import Chart from "./Chart.svelte";
-	import Hello from "./Hello.svelte";
-	var response={};
-
-	const ticker = "X:BTCUSD";
-	
-	onMount(async () => {
-	  //response = await fetch("https://us-central1-frontier-eb43f.cloudfunctions.net/stats?ticker="+ticker);
-	  response = await fetch("http://localhost:5001/frontier-eb43f/us-central1/stats?ticker="+ticker); 
-	  response = response.json(); 
-
-	});
-
-
+	import AssetCard from "./AssetCard.svelte";
 </script>
 
 <main>
-	{#await response then response}
-		<h1>{ticker}</h1>
-		<h2>Last 30 days analysis</h2>
-		<div>
-			<p>OPEN  :{response.opening}</p>
-			<p>CLOSE :{response.closing}</p>
-			<p>RETURN:{response.return}</p>
-			<p>VOLATILITY :{response.stdev}</p>
-		</div>
-		
-		<Chart points={response.points}></Chart>
-	  	
-			
-	{/await}
+	<AssetCard ticker="X:BTCUSD"></AssetCard>
 </main>
---
+++ frontend-svelte/my-svelte-project/src/AssetCard.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { onMount, tick } from "svelte";
+    import Chart from "./Chart.svelte";
+	var response={};
+
+	export let ticker;
+	
+	onMount(async () => {
+	  //response = await fetch("https://us-central1-frontier-eb43f.cloudfunctions.net/stats?ticker="+ticker);
+	  response = await fetch("http://localhost:5001/frontier-eb43f/us-central1/stats?ticker="+ticker); 
+	  response = response.json(); 
+
+	});
+
+
+</script>
+
+<div class="card">
+	{#await response then response}
+		<h1>{ticker}</h1>
+		<h2>Last 30 days analysis</h2>
+		<div>
+			<p>OPEN  :{response.opening}</p>
+			<p>CLOSE :{response.closing}</p>
+			<p>RETURN:{response.return}</p>
+			<p>VOLATILITY :{response.stdev}</p>
+		</div>
+		
+		<Chart points={response.points}></Chart>
+	  	
+			
+	{/await}
+    </div>
+
+
+    <style>
+        .card {
+            width: 300px;
+            border: 1px solid #aaa;
+            border-radius: 2px;
+            box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+            padding: 1em;
+            margin: 0 0 1em 0;
+        }
+    </style>
