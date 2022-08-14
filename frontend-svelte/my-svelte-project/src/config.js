c11714723fecf9b516e3f081aabaaea2610e8ab2
davide <davide@Mini-di-davide.lan>
2022-08-14 16:29:41 +0200

    big refact

--- frontend-svelte/my-svelte-project/src/config.js
+++
@@ -1,5 +0,0 @@
-//exports.baseurl = "http://localhost:5001/frontier-eb43f/us-central1"
-//exports.baseurl = "http://localhost:5001/frontier-eb43f/us-central1/prices?ticker=";
-exports.baseurl = "https://us-central1-frontier-eb43f.cloudfunctions.net/prices?ticker=";
-//exports.baseurl = "https://firebasestorage.googleapis.com/v0/b/frontier-eb43f.appspot.com/o/.cache%2F";
-
