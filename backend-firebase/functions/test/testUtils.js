c92b014f5aab5acaa122184e1f1e1033fb6815aa
davide <davide@Mini-di-davide.lan>
2022-08-14 13:08:50 +0200

    added eth

--- backend-firebase/functions/test/testUtils.js
+++ backend-firebase/functions/test/testUtils.js
@@ -1,14 +1,9 @@
 // test/converter.js
 
 var {expect}    = require("chai");
-var {priceHistory, writePrices, readPrices, writeAll} = require("../utils.js");
+var {writePrices, readPrices, writeAll} = require("../utils.js");
 
 describe("Utils", function() {
-
-    it("fecth price history", async function() {
-        let h = await priceHistory("NDAQ",90);
-        console.log(h);
-    });
 
     it("write/read price history to storage", async function() {
         let obj = {"x":123};
